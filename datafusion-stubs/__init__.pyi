3ee25eab0244e6e2c865798e4b9744ef241ec31d
Reda Bouaida <reda.bouaida@outlook.com>
2024-05-05 10:10:04 +0200

    Prepare for v0.0.5

--- datafusion-stubs/__init__.pyi
+++ datafusion-stubs/__init__.pyi
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import abc
+
 from typing import Any
 from typing import Callable
 from typing import TYPE_CHECKING
@@ -7,16 +9,83 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     import pyarrow as pa
 
+from datafusion._internal import AggregateUDF as AggregateUDF
+from datafusion._internal import Config as Config
+from datafusion._internal import DataFrame as DataFrame
+from datafusion._internal import SessionContext as SessionContext
+from datafusion._internal import SessionConfig as SessionConfig
+from datafusion._internal import RuntimeConfig as RuntimeConfig
+from datafusion._internal import ScalarUDF as ScalarUDF
+from datafusion._internal import SQLOptions as SQLOptions
+
+from datafusion.common import DFField as DFField
+from datafusion.common import DFSchema as DFSchema
+
 from datafusion.expr import Expr
+from datafusion.expr import Alias as Alias
+from datafusion.expr import Analyze as Analyze
+from datafusion.expr import Expr as Expr
+from datafusion.expr import Filter as Filter
+from datafusion.expr import Limit as Limit
+from datafusion.expr import Like as Like
+from datafusion.expr import ILike as ILike
+from datafusion.expr import Projection as Projection
+from datafusion.expr import SimilarTo as SimilarTo
+from datafusion.expr import ScalarVariable as ScalarVariable
+from datafusion.expr import Sort as Sort
+from datafusion.expr import TableScan as TableScan
+from datafusion.expr import GetIndexedField as GetIndexedField
+from datafusion.expr import Not as Not
+from datafusion.expr import IsNotNull as IsNotNull
+from datafusion.expr import IsTrue as IsTrue
+from datafusion.expr import IsFalse as IsFalse
+from datafusion.expr import IsUnknown as IsUnknown
+from datafusion.expr import IsNotTrue as IsNotTrue
+from datafusion.expr import IsNotFalse as IsNotFalse
+from datafusion.expr import IsNotUnknown as IsNotUnknown
+from datafusion.expr import Negative as Negative
+from datafusion.expr import ScalarFunction as ScalarFunction
+from datafusion.expr import BuiltinScalarFunction as BuiltinScalarFunction
+from datafusion.expr import InList as InList
+from datafusion.expr import Exists as Exists
+from datafusion.expr import Subquery as Subquery
+from datafusion.expr import InSubquery as InSubquery
+from datafusion.expr import ScalarSubquery as ScalarSubquery
+from datafusion.expr import GroupingSet as GroupingSet
+from datafusion.expr import Placeholder as Placeholder
+from datafusion.expr import Case as Case
+from datafusion.expr import Cast as Cast
+from datafusion.expr import TryCast as TryCast
+from datafusion.expr import Between as Between
+from datafusion.expr import Explain as Explain
+from datafusion.expr import CreateMemoryTable as CreateMemoryTable
+from datafusion.expr import SubqueryAlias as SubqueryAlias
+from datafusion.expr import Extension as Extension
+from datafusion.expr import CreateView as CreateView
+from datafusion.expr import Distinct as Distinct
+from datafusion.expr import DropTable as DropTable
+from datafusion.expr import Repartition as Repartition
+from datafusion.expr import Partitioning as Partitioning
+from datafusion.expr import Window as Window
+from datafusion.expr import WindowFrame as WindowFrame
+
+
 from datafusion._internal import ScalarUDF
 from datafusion._internal import AggregateUDF
 from datafusion._internal import DataFrame as DataFrame
 from datafusion._internal import SessionContext as SessionContext
 
-class Accumulator:
+
+__version__: str
+
+class Accumulator(abc.ABC):
+    @abc.abstractmethod
     def state(self) -> list[pa.Scalar[Any]]: ...
+    @abc.abstractmethod
     def update(self, values: pa.Array[Any, Any]) -> None: ...
+    @abc.abstractmethod
     def merge(self, states: pa.Array[Any, Any]) -> None: ...
+    @abc.abstractmethod
     def evaluate(self) -> pa.Scalar[Any]: ...
 
 def column(value: str) -> Expr: ...
--- datafusion-stubs/_internal/substrait.pyi
+++ datafusion-stubs/_internal/substrait.pyi
@@ -8,24 +8,133 @@ if TYPE_CHECKING:
     from datafusion._internal.sql.logical import LogicalPlan
 
 class plan:
-    def encode(self) -> Any: ...
+    def encode(self) -> bytes:
+        """Encode the plan to bytes.
+
+        Returns
+        -------
+        bytes
+            Encoded plan.
+        """
+        ...
 
 class serde:
     @staticmethod
-    def serialize(sql: str, ctx: SessionContext, path: str) -> None: ...
+    def serialize(sql: str, ctx: SessionContext, path: str) -> None:
+        """Serialize a SQL query to a Substrait plan and write it to a file.
+
+        Parameters
+        ----------
+        sql : str
+            SQL query to serialize.
+        ctx : SessionContext
+            SessionContext to use.
+        path : str
+            Path to write the Substrait plan to.
+        """
+        ...
+
     @staticmethod
-    def serialize_to_plan(sql: str, ctx: SessionContext) -> plan: ...
+    def serialize_to_plan(sql: str, ctx: SessionContext) -> plan:
+        """Serialize a SQL query to a Substrait plan.
+
+        Parameters
+        ----------
+        sql : str
+            SQL query to serialize.
+        ctx : SessionContext
+            SessionContext to use.
+
+        Returns
+        -------
+        plan
+            Substrait plan.
+        """
+        ...
+
     @staticmethod
-    def serialize_bytes(sql: str, ctx: SessionContext) -> bytes: ...
+    def serialize_bytes(sql: str, ctx: SessionContext) -> bytes:
+        """Serialize a SQL query to a Substrait plan as bytes.
+
+        Parameters
+        ----------
+        sql : str
+            SQL query to serialize.
+        ctx : SessionContext
+            SessionContext to use.
+
+        Returns
+        -------
+        bytes
+            Substrait plan as bytes.
+        """
+        ...
+        
     @staticmethod
-    def deserialize(path: str) -> plan: ...
+    def deserialize(path: str) -> plan:
+        """Deserialize a Substrait plan from a file.
+
+        Parameters
+        ----------
+        path : str
+            Path to read the Substrait plan from.
+
+        Returns
+        -------
+        plan
+            Substrait plan.
+        """
+        ...
     @staticmethod
-    def deserialize_bytes(proto_bytes: bytes) -> plan: ...
+    def deserialize_bytes(proto_bytes: bytes) -> plan:
+        """Deserialize a Substrait plan from bytes.
+
+        Parameters
+        ----------
+        proto_bytes : bytes
+            Bytes to read the Substrait plan from.
+
+        Returns
+        -------
+        plan
+            Substrait plan.
+        """
+        ...
 
 class producer:
     @staticmethod
-    def to_substrait_plan(plan: LogicalPlan, ctx: SessionContext) -> plan: ...
+    def to_substrait_plan(plan: LogicalPlan, ctx: SessionContext) -> plan:
+        """Convert a DataFusion LogicalPlan to a Substrait plan.
+
+        Parameters
+        ----------
+        plan : LogicalPlan
+            LogicalPlan to convert.
+        ctx : SessionContext
+            SessionContext to use.
+
+        Returns
+        -------
+        plan
+            Substrait plan.
+        """
+        ...
 
 class consumer:
     @staticmethod
-    def from_substrait_plan(plan: plan, ctx: SessionContext) -> LogicalPlan: ...
+    def from_substrait_plan(ctx: SessionContext, plan: plan) -> LogicalPlan:
+        """Convert a Substrait plan to a DataFusion LogicalPlan.
+
+        Parameters
+        ----------
+        ctx : SessionContext
+            SessionContext to use.
+        plan : plan
+            Substrait plan to convert.
+
+        Returns
+        -------
+        LogicalPlan
+            LogicalPlan.
+        """
+        ...
